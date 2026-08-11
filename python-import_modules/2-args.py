49cb47b402b49eb23d2bf4094f3d66cecedd175c
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-08-11 08:09:38 +0400

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    number = len(sys.argv) - 1
+
+    if number == 0:
+        print("0 arguments.")
+    elif number == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(number))
+
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
