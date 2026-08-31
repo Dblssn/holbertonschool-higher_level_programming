3ab89f078386a8835bc88396cff7dc67a78a297b
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-08-31 13:36:23 +0400

    task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if type(roman_string) is str:
+        values = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        total = 0
+        previous = 0
+
+        for char in reversed(roman_string.upper()):
+            current = values.get(char, 0)
+
+            if current >= previous:
+                total += current
+            else:
+                total -= current
+
+            previous = current
+
+        return (total)
+    return (previous)
