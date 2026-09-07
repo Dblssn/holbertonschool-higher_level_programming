f06dfd54f6d49876ab9783b16c5ad44ff3c2b79a
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-09-07 13:38:23 +0400

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
 def safe_print_list(my_list=[], x=0):
+    i = 0
     result = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            result = i + 1
+            result += 1
         except IndexError:
             break
 
