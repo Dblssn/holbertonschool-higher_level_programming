dc9831a0bea7104ded3970ca01c7d4ee4a63975c
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-09-14 14:12:12 +0400

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,13 +4,18 @@
 this function adds 2 integers or floats
 It returns an addition of a and b
 """
+
+
 def add_integer(a, b=98):
     """ Add 2 integer
-    
+
     """
-    if type(a) is not (int) and type(a) is not (float):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) is not (int) and type(b) is not (float):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    
-    return int(a + b)
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
