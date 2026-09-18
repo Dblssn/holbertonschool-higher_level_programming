02f680c43737d79d6409a78f7cd31cf1bf80fb78
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-09-18 08:40:24 +0400

    task 4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,13 +4,18 @@
 
 
 def text_indentation(text):
-    """This function prints a text with 2 new lines
-    after each of these char:., ? and:
-    if the text is not a string a TypeError will be raised
+    """This function prints text with 2 new lines after ., ? and :.
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    text = text.replace(".", ".\n\n")
-    text = text.replace(":", ":\n\n")
-    text = text.replace("?", "?\n\n")
-    print(text, end="")
+
+    line = ""
+    for char in text:
+        line += char
+        if char in [".", "?", ":"]:
+            print(line.strip())
+            print()
+            line = ""
+
+    if line.strip():
+        print(line.strip(), end="")
