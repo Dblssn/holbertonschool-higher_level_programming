f472d4b3240d62ffb599d85d1b668753d31ff5fe
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-09-21 13:11:09 +0400

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a function that write a class
+"""
+
+
+class Square:
+    def __init__(self, size):
+        """This function defines a square
+        """
+        self.__size = size
