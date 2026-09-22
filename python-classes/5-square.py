859fd09fc2d1d5c13888720ff33c6a563316ff9b
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-09-22 10:10:23 +0400

    task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -27,5 +27,8 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
-        for i in range(self.__size):
-            print("#" * self.__size)
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
